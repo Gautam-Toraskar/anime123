d44aa7241c46d6678be88c91f908a36ff1d442c1
Gautam-Toraskar <gtmtoraskar19@gmail.com>
2014-07-03 18:57:15 +0530

    changed animae part on hello.php page

---
+++ hello.php
@@ -0,0 +1,298 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Careers | Home</title>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+    <META HTTP-EQUIV="Content-language" CONTENT="tr">
+    <!-- Bootstrap -->
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/packages/font-awesome/assets/css/font-awesome-ie7.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/packages/font-awesome/assets/css/admin/editor_styles.css">
+    <link rel="stylesheet" type="text/css" href="assets/packages/mm-navbar/jquery.mmenu.all.css"/>
+    <script type="text/javascript" src="assets/packages/font-awesome/assets/js/font-awesome.js"></script>
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lato:300,400,700,400italic">
+
+    <!-- fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="assets/fonts/proxima-nova/proxima-nova.css">
+    <script type="text/javascript" src="assets/fonts/proxima-nova/proxima-nova.js"></script>
+    <link href="http://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900" rel="stylesheet" type="text/css">
+
+    <!-- <link rel="stylesheet" type="text/css" href="assets/fonts/Raleway-regular/stylesheet.css"> -->
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+  </head>
+  <body id="body">
+
+    <nav id="menu" class="mm-menu">
+       <div style="padding-top: 20px;">
+          <ul class="List">
+            <li class="Selected"><a href="#intro-banner">Biz Kimiz</a></li>
+            <li><a href="#services">Ne Sunuyoruz</a></li>
+            <li><a href="#contact">Başlayın</a></li>
+          </ul>
+       </div>
+    </nav>
+
+    <div class="page">
+      <header class="fixed">
+        <div class="container">
+          <div class="logo">
+            <div class="logo-icon"></div>
+            <!-- <img src="assets/img/codfabrik.svg"> -->
+          </div>
+          <style type="text/css">
+            a[href='#menu']{
+              overflow: hidden;
+              height: 31px;
+              width: 31px;
+              margin: 12px 0;
+              float: left;
+            }
+          </style>
+          <!-- <a href="#menu"><span class="icon-reorder" style="font-size:30px;color:white;"></span></a> -->
+
+          <nav class="nav">
+            <div>
+              <ul class="list">
+                <li class="selected">
+                  <a href="#intro-banner">
+                    <h3>Company</h3>
+                  </a>
+                </li>
+                <li>
+                  <a href="#services">
+                    <h3>Services</h3>
+                  </a>
+                </li>
+                <li>
+                  <a href="#contact">
+                    <h3>Enterprise</h3>
+                  </a>
+                </li>
+                <li>
+                  <a href="#contact">
+                    <h3>Work</h3>
+                  </a>
+                </li>
+                <li>
+                  <a href="#contact">
+                    <h3>Contact</h3>
+                  </a>
+                </li>
+                <li>
+                  <a href="#contact">
+                    <h3>Careers</h3>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </header>
+      <style type="text/css">
+
+
+      </style>
+      <section id="hero">
+        <div class="container">
+          <h1 class="page-title">Working At Twitter</h1>
+          <h2 class="subtitle">Help us to build real-time products that have global impact.</h2>
+
+        </div>
+        <div class="careers-jobs-nav">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="#"><h2>Jobs</h2></a>
+            </div>
+            <div class="col-md-6">
+              <a href="#"><h2>Internship</h2></a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <style type="text/css">
+/*        .content-box-margin-left220{
+          margin-left: 220px;
+        }*/
+
+        .container .row .element-content:nth-child(odd){
+          padding-left: 210px; 
+        }
+      </style>
+      <section id="core">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 ">
+              <div class="element-box pull-right">
+                <div class="lightening-anime">
+                  <div class="light-rod-1 light-rod"></div>
+                  <div class="light-rod-2 light-rod"></div>
+                  <div class="light-rod-3 light-rod"></div>
+                  <div class="light-rod-4 light-rod"></div>
+                  <div class="light-rod-5 light-rod"></div>
+                  <div class="hand"></div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 element-content">
+              <h3>Build Solutions that Reach</h3>
+              <p>
+                To delight clients, transform the energy<br>and defne new possibilities.
+              </p>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-6 element-content ">
+              <h3>Design First</h3>
+              <p>
+                Design comes to life. Always thinking<br/>that the interface is the application,<br/>we devote sumptuous attention to intricate<br/> details. 
+              </p>
+            </div>
+            <div class="col-md-6 ">
+              <div class="element-box pull-left">
+                <div class="pencil-brain">
+                  <div class="brain"></div>
+                  <div class="pencil"></div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-6 ">
+              <div class="element-box pull-right">
+                <div class="box-bulb">
+                  <div class="bulb-rays"></div>
+                  <div class="bulb"></div>
+                  <div class="hand-box"></div>
+                </div> 
+              </div>
+            </div>
+            <div class="col-md-6 element-content">
+              <h3>Innovation Always</h3>
+              <p>
+                We will continuously bring you new ways of <br/>thinking that promotes ideas that are fresh,<br/>on-trend and relevantto blend traditional <br/>methodologies with emerging technologies <br/>and new media.
+              </p>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-6 element-content">
+              <h3>Be fast Be good</h3>
+              <p>
+                We are a small team and hence very agile<br>and fexible. So we make the “go-to market”<br>process quick, efcient and afordable.
+              </p>
+            </div>
+            <div class="col-md-6 ">
+              <div class="element-box pull-left">
+                <div class="icon-box">
+                  <div class="meteo"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <style type="text/css">
+        section h2{
+          color: gray;
+          font-family: "raleway",sans-serif;
+          font-size: 55px;
+          font-weight: 300;
+        }
+        section#openings div.container-smooth{
+          background-color: #5fb336;
+          min-height: 100px;
+        }
+        section#openings div.container-smooth .container{
+          padding-top: 20px;
+          padding-bottom: 90px;
+        }
+        section#openings div.careersPage{
+          background-repeat: no-repeat;
+          background-image: url(assets/img/careersPage.png);
+          transition: all 0.2s ease-in-out;
+        }
+        .careersOpenings h3{
+          font-size: 28px;
+          color: white;
+          margin-top: 278px;
+          text-align: center;
+          font-family: "raleway",sans-serif;
+        }
+      </style>
+      <section id="openings">
+      <h2 class="text-center">Browse openings by Team</h2>
+        <div class="container-smooth">
+          <div class="container">
+            <div class="row">
+              <div id="software-engg" class="col-md-3 careersOpenings sw">
+                <h3>Software Enginnering</h3>
+              </div>
+              <div id="design" class="col-md-3 careersOpenings design">
+                <h3>Design &amp; UX Research</h3>
+              </div>
+              <div id="marketing" class="col-md-3 careersOpenings marketing">
+                <h3>Business &amp; Market Development</h3>
+              </div>
+              <div id="HR" class="col-md-3 careersOpenings hr">
+                <h3>HR &amp; Recruiting</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <style type="text/css">
+        #techs .techs{
+          margin: auto;
+          cursor: pointer;
+        }
+        #techs .tech-wrapper{
+          padding: 20px 0;
+        }
+      </style>
+      <section id="techs">
+        <!-- <h2 class="text-center">Technologies</h2> -->
+        <div class="container">
+          <div class="row tech-wrapper">
+            <div class="col-md-3">
+              <div class="techs node_bw"></div>
+            </div>
+            <div class="col-md-3">
+              <div class="techs coffee_bw"></div>
+            </div>
+            <div class="col-md-3">
+              <div class="techs html5_bw"></div>
+            </div>
+            <div class="col-md-3">
+              <div class="techs css3_bw"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <?php require_once("views/footer.php"); ?>
+    </div> 
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!-- // <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> -->
+    <script src="assets/js/jquery-1.11.0.min.js" type="text/javascript"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/main.js"></script>
+  </body>
+</html>
