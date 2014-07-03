340abda06c0cf5073f598c13647d790a127111c7
Gautam-Toraskar <gtmtoraskar19@gmail.com>
2014-07-03 14:57:42 +0530

    footer finaised.

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/////////////////////navbar//////////////////////////
+
+$(function() {
+  $('nav#menu').mmenu({
+    classes: 'mm-light',
+    searchfield : true ,
+    counters    : true 
+  });
+});
